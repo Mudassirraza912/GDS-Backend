0feae0e6f8dec91d509e06b951ce854b1269cab2
Mudassirraza912 <razamudassir912@gmail.com>
2025-07-11 16:03:33 +0500

    feat: Configure CORS with specific origin and credentials

--- src/index.js
+++ src/index.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 // Configure CORS for your React app
-app.use(cors());
+app.use(cors({
+  origin: 'https://mudassirs-gds-mvp-frontend.vercel.app',
+  credentials: true,
+}));
+app.options('*', cors());
 
 // Set up session middleware BEFORE your routes
 app.use(session({
